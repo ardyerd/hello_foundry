5e6d4949cc73674681797491cb780e8edea50dcc
ardyerd <ardy.erdiyanto@gmail.com>
2024-11-02 11:16:31 +0700

    initial commit

---
+++ src/Counter.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 public number;
+    uint256 constant public totalSupply = 100;
+
+    uint256 public price;
+
+    address public owner;
+
+    constructor(uint _price) {
+        owner = msg.sender;
+        price = _price;
+    }
+
+    function setPrice(uint256 newPrice) public {
+        require(msg.sender == owner, "Only owner can set price");
+        price = newPrice;
+    }
+
+    function setOwner(address newOwner) public {
+        require(msg.sender == owner, "Only owner can set price");
+        owner = newOwner;
+    }
+
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+
+}
---
+++ test/Counter.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract CounterTest is Test {
+    Counter public counter;
+
+    function setUp() public {
+        counter = new Counter(100);
+        counter.setNumber(0);
+    }
+
+    function test_Increment() public {
+        counter.increment();
+        assertEq(counter.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        counter.setNumber(x);
+        assertEq(counter.number(), x);
+    }
+
+    function test_TotalSupply() public {
+        console.log(counter.totalSupply());
+        assertEq(counter.totalSupply(), 100);
+    }
+
+    function test_SetPrice() public {
+        console.log(counter.owner());
+        counter.setPrice(100);
+        assertEq(counter.price(), 100);
+    }
+
+    function test_Add() public {
+        assertEq(counter.add(1, 2), 3);
+    }
+
+    function test_SetOwner() public {
+        address alice = address(1);
+        counter.setOwner(alice);
+        assertEq(counter.owner(), alice);
+
+        // ubah Price with new Owner
+        vm.expectRevert("Only owner can set price");
+        counter.setPrice(200);
+
+        vm.prank(alice);
+        counter.setPrice(300);
+        assertEq(counter.price(), 300);
+    }
+
+}
